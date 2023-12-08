ea4e08bdc7e27969e6d53fbd2cdaf749529afa0a
karissakwan <122500371+karissakwan@users.noreply.github.com>
2023-12-07 23:52:22 -0800

    Add files via upload

---
+++ lambda_function.py
@@ -0,0 +1,44 @@
+import json
+import boto3
+
+def lambda_handler(event, context):
+    
+    # connect to DynamoDB resource
+    client = boto3.resource('dynamodb')
+    
+    #create a DynamoDB client to the visitor_count table
+    table = client.Table('visitor_count')
+    
+    #increment visitor_count attribute for index.html key
+    
+    response = table.update_item(
+        Key={
+            'path': 'index.html'
+        },
+        AttributeUpdates={
+            'visitor_count':{
+                'Value' : 1,
+                'Action': 'ADD'
+            }
+        }
+    )
+    
+   
+    
+    #get updated visitor_count
+    
+    #return visitor_count to user
+    response = table.get_item(
+        Key={
+            'path': 'index.html'
+        }
+    )
+    visitor_count = response['Item']['visitor_count']
+    
+    return {
+        'statusCode': 200,
+        'header': {
+            'Access-Control-Allow-Origin' : '*'
+        },
+        'body': visitor_count
+    }
